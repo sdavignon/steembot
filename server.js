236ed4e5d30b7ae13da1a44f05445d6967d3dc24
sdavignon <sdavignon1@gmail.com>
2018-03-11 14:34:49 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+const { url } = require('url');
+var steem = require('steem');
+
+app.set('port', (process.env.PORT || 2222)) // Define Port
+app.use(express.static(__dirname + '/public')) // Define static public folder
+
+var steembot_id = process.env.STREEMBOT_ID;
+var voter = process.env.STEEMVOTER;
+var wif = process.env.STEEMVOTERWIF;
+
+
+var permlinkFix = new Date().toISOString().replace(/[^a-zA-Z0-9]+/g, '').toLowerCase();
+
+
+
+
+
+// write an index file to be served when they access steemblast.com
+app.get('/', (req, res, next) => {
+    res.send('public/index.html')
+    });
+	
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res, next){
+   res.send('public/index.html')
+ });
+
+
+
+app.listen(app.get('port'), function() {
+  console.log("steembot is live on:" + app.get('port'))
+})
